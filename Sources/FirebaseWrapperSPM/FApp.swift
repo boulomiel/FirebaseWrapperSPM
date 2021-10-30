114756ee5ed86ed4cd4d2d0fb28ddbdedf8aa5e8
Ruben <ruben.mimoun@gmail.com>
2021-10-30 18:10:21 +0300

    FApp file added

---
+++ Sources/FirebaseWrapperSPM/FApp.swift
@@ -0,0 +1,18 @@
+//
+//  FirebaseApp.swift
+//  FirebaseWrapper
+//
+//  Created by Ruben Mimoun on 24/09/2021.
+//
+
+import Foundation
+import Firebase
+
+public struct FirebaseWrapper{
+    
+    public init() {
+        FirebaseApp.configure()
+    }
+
+   
+}
